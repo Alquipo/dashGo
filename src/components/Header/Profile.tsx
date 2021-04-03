020990a7f77f3ce771bec5f6e21e8f3067863e91
Alquipo <alquiponeto@outlook.com.br>
2021-04-03 11:48:51 -0300

    fix: header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,13 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+export function Profile({ showProfileData = true }:ProfileProps) {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
-        <Text>Alquipo Neto</Text>
-        <Text color='gray.300' fontSize='small'>alquiponet@outlook.com.br</Text>
-      </Box>
-
+      {showProfileData && (
+        <Box mr='4' textAlign='right'>
+          <Text>Alquipo Neto</Text>
+          <Text color='gray.300' fontSize='small'>alquiponet@outlook.com.br</Text>
+        </Box>
+      )}
+      
       <Avatar size='md' name='Alquipo Neto' src="https://github.com/alquipo.png"/>
   </Flex>
   )
